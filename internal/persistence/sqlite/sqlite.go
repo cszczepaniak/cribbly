2ed3da15f8bbb08c4ecd06c5a02487765a950f4b
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-04-19 10:15:05 -0500

    allow (and default to) using a file for sqlite persistence

--- internal/persistence/sqlite/sqlite.go
+++ internal/persistence/sqlite/sqlite.go
@@ -2,8 +2,19 @@ package sqlite
 
 import (
 	"database/sql"
+	"os"
+	"path/filepath"
 )
 
 func NewInMemory() (*sql.DB, error) {
 	return sql.Open("libsql", "file::memory:")
+}
+
+func NewFromFile(path string) (*sql.DB, error) {
+	err := os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	return sql.Open("libsql", "file:"+path)
 }
