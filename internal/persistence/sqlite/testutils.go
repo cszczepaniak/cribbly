285fd3539a8f6b4cf96939622162233ef6f38f36
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-04-21 18:55:55 -0500

    use a non-cgo-dependent sqlite driver

--- internal/persistence/sqlite/testutils.go
+++ internal/persistence/sqlite/testutils.go
@@ -6,9 +6,7 @@ import (
 	"database/sql"
 	"testing"
 
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/require"
-	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func NewInMemoryForTest(t testing.TB) *sql.DB {
