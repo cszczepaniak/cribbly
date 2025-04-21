7b5ebf213d5e907b4b1fe1bc0fa5bad8fa0a1689
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-04-20 19:45:47 -0500

    TeamService: add + test teams DB service

---
+++ internal/persistence/teams/teams_test.go
@@ -0,0 +1,53 @@
+package teams
+
+import (
+	"testing"
+
+	"github.com/cszczepaniak/cribbly/internal/persistence/sqlite"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTeamsService(t *testing.T) {
+	db := sqlite.NewInMemoryForTest(t)
+	s := NewService(db)
+	require.NoError(t, s.Init(t.Context()))
+
+	team1, err := s.Create(t.Context())
+	require.NoError(t, err)
+	team2, err := s.Create(t.Context())
+	require.NoError(t, err)
+	team3, err := s.Create(t.Context())
+	require.NoError(t, err)
+
+	teams, err := s.GetAll(t.Context())
+	assert.ElementsMatch(t, []Team{team1, team2, team3}, teams)
+
+	team, err := s.Get(t.Context(), team1.ID)
+	require.NoError(t, err)
+	assert.Equal(t, team1, team)
+
+	team, err = s.Get(t.Context(), team2.ID)
+	require.NoError(t, err)
+	assert.Equal(t, team2, team)
+
+	team, err = s.Get(t.Context(), team3.ID)
+	require.NoError(t, err)
+	assert.Equal(t, team3, team)
+}
+
+func TestTeamsService_Rename(t *testing.T) {
+	db := sqlite.NewInMemoryForTest(t)
+	s := NewService(db)
+	require.NoError(t, s.Init(t.Context()))
+
+	team, err := s.Create(t.Context())
+	require.NoError(t, err)
+	assert.Equal(t, "Unnamed Team", team.Name)
+
+	require.NoError(t, s.Rename(t.Context(), team.ID, "New Name"))
+
+	gotTeam, err := s.Get(t.Context(), team.ID)
+	assert.Equal(t, team.ID, gotTeam.ID)
+	assert.Equal(t, "New Name", gotTeam.Name)
+}
