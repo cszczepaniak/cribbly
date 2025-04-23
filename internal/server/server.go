d08601d6113436000849626bff236d6174c33781
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-04-22 21:02:01 -0500

    this is nearly impossible.

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,17 +15,17 @@ func Setup() http.Handler {
 	mux.Handle("GET /", components.Handle(index.Index))
 	mux.Handle("GET /list", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		items := []string{
-			`Item 1`,
-			`Item 2`,
-			`Item 3`,
-			`Item 4`,
+			`item1`,
+			`item2`,
+			`item3`,
+			`item4`,
 		}
 		rand.Shuffle(len(items), func(i, j int) {
 			items[i], items[j] = items[j], items[i]
 		})
 		w.Write([]byte(`<ul id="items">`))
 		for _, item := range items {
-			fmt.Fprintf(w, `<li>%s</li>`, item)
+			fmt.Fprintf(w, `<li class="smooth" id="%s">%s</li>`, item, item)
 		}
 		w.Write([]byte(`</ul>`))
 	}))
