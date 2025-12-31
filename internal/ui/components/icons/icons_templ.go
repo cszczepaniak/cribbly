f6f936cec1cfef27faa801e2458bfd08a0553ce7
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-12-30 21:19:25 -0600

    format everything again

--- internal/ui/components/icons/icons_templ.go
+++ internal/ui/components/icons/icons_templ.go
@@ -5,10 +5,12 @@ package icons
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import "github.com/a-h/templ"
-import templruntime "github.com/a-h/templ/runtime"
+import (
+	"fmt"
 
-import "fmt"
+	"github.com/a-h/templ"
+	templruntime "github.com/a-h/templ/runtime"
+)
 
 // These icons are sourced from https://flowbite.com/icons/
 // Icon size: 24
