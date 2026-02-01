495061f94731be2b7e09db692d5bcd1071d267bd
Connor Szczepaniak <cszczepaniak@gmail.com>
2026-01-31 19:26:22 -0600

    add a config package

---
+++ internal/config/config.go
@@ -0,0 +1,92 @@
+package config
+
+import (
+	"encoding"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+type Config struct {
+	DSN      string `env:"DSN"`
+	SeedUser struct {
+		SeedUsername string `env:"SEED_USERNAME"`
+		SeedPassword string `env:"SEED_PASSWORD"`
+	}
+}
+
+func Load(cfg *Config) error {
+	val := reflect.ValueOf(cfg).Elem()
+	return populate(val)
+}
+
+func populate(val reflect.Value) error {
+	if val.Kind() != reflect.Pointer {
+		return errors.New("value must be a pointer")
+	}
+
+	typ := val.Elem().Type()
+	for i := range val.Elem().NumField() {
+		field := val.Elem().Field(i)
+		if field.Kind() == reflect.Struct {
+			err := populate(field.Addr())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		envVarName := typ.Field(i).Tag.Get("env")
+		if envVarName == "" {
+			continue
+		}
+
+		envVal, ok := os.LookupEnv(envVarName)
+		if !ok {
+			continue
+		}
+
+		err := setFromEnv(field, envVal)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func setFromEnv(val reflect.Value, envVal string) error {
+	tu, ok := val.Interface().(encoding.TextUnmarshaler)
+	if ok {
+		return tu.UnmarshalText([]byte(envVal))
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		val.SetString(envVal)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		i, err := strconv.ParseInt(envVal, 10, 64)
+		if err != nil {
+			return err
+		}
+		val.SetInt(i)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		i, err := strconv.ParseUint(envVal, 10, 64)
+		if err != nil {
+			return err
+		}
+		val.SetUint(i)
+	case reflect.Bool:
+		b, err := strconv.ParseBool(envVal)
+		if err != nil {
+			return err
+		}
+		val.SetBool(b)
+	default:
+		return fmt.Errorf("unsupported config kind: %v", val.Kind())
+	}
+
+	return nil
+}
---
+++ internal/config/config_test.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testConfig struct {
+	MyStr    string `env:"MY_STR"`
+	MyInt    int    `env:"MY_INT"`
+	MyUint   uint   `env:"MY_UINT"`
+	MyBool   bool   `env:"MY_BOOL"`
+	MyStruct struct {
+		Nested1        string `env:"NESTED_1"`
+		MyNestedStruct struct {
+			Nested2 string `env:"NESTED_2"`
+		}
+	}
+}
+
+func TestPopulate(t *testing.T) {
+	t.Setenv("MY_STR", "str1")
+	t.Setenv("MY_INT", "123")
+	t.Setenv("NESTED_1", "nested1")
+	t.Setenv("NESTED_2", "nested2")
+	cfg := &testConfig{}
+
+	require.NoError(t, populate(reflect.ValueOf(cfg)))
+
+	assert.Equal(t, "str1", cfg.MyStr)
+	assert.Equal(t, 123, cfg.MyInt)
+	assert.Equal(t, uint(0), cfg.MyUint) // env not set
+	assert.Equal(t, false, cfg.MyBool)   // env not set
+	assert.Equal(t, "nested1", cfg.MyStruct.Nested1)
+	assert.Equal(t, "nested2", cfg.MyStruct.MyNestedStruct.Nested2)
+
+	t.Setenv("MY_UINT", "124")
+	t.Setenv("MY_BOOL", "true")
+
+	cfg = &testConfig{}
+
+	require.NoError(t, populate(reflect.ValueOf(cfg)))
+
+	assert.Equal(t, "str1", cfg.MyStr)
+	assert.Equal(t, 123, cfg.MyInt)
+	assert.Equal(t, uint(124), cfg.MyUint)
+	assert.Equal(t, true, cfg.MyBool)
+	assert.Equal(t, "nested1", cfg.MyStruct.Nested1)
+	assert.Equal(t, "nested2", cfg.MyStruct.MyNestedStruct.Nested2)
+}
