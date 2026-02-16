4683379f15945b28897b7f27217a8cb444d07065
Connor Szczepaniak <cszczepaniak@gmail.com>
2026-02-15 20:16:25 -0600

    make in-mem DB work better

--- internal/persistence/database/testutils.go
+++ internal/persistence/database/testutils.go
@@ -1,13 +1,20 @@
 package database
 
 import (
+	"database/sql"
 	"testing"
+
+	_ "github.com/ncruces/go-sqlite3/driver"
+	_ "github.com/ncruces/go-sqlite3/embed"
+	"github.com/ncruces/go-sqlite3/vfs/memdb"
 
 	"github.com/cszczepaniak/cribbly/internal/assert"
 )
 
 func NewInMemory(t *testing.T) Database {
-	db, err := NewSQLiteDB("file::memory:")
+	// See https://pkg.go.dev/github.com/ncruces/go-sqlite3/vfs/memdb#example-package
+	memdb.Create("test.db", nil)
+	db, err := sql.Open("sqlite3", "file:/test.db?vfs=memdb")
 	assert.NoError(t, err)
-	return db
+	return New(db)
 }
