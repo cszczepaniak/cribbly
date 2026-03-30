43a02fff4fe1d3d61599c7fcf2e9af731860221c
Connor Szczepaniak <cszczepaniak@gmail.com>
2026-03-29 21:31:44 -0500

    format

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
-non_templ_go_files=$(find . -name "*.go" -not -name "*templ.go" -not -name "*.pb.go" -not -name "*.connect.go" | grep -v templui)
+non_templ_go_files=$(find . \
+	-name "*.go" \
+	-not -name "*templ.go" \
+	-not -name "*.pb.go" \
+	-not -name "*.connect.go" | grep -v templui)
 goimports -w -l -local github.com/cszczepaniak/cribbly $non_templ_go_files
