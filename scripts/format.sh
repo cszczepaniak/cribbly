e9467e745fbb610564b6f6b8f1cf1244c47d5790
Connor Szczepaniak <cszczepaniak@gmail.com>
2026-03-29 21:34:23 -0500

    fix format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-non_templ_go_files=$(find . \
+non_templ_go_files=$(find . \( \
+		-path ./vendor -o \
+		-path ./internal/ui/components/templui \
+	\) -prune -o \
+	-type f \
 	-name "*.go" \
 	-not -name "*templ.go" \
 	-not -name "*.pb.go" \
-	-not -name "*.connect.go" | grep -v templui)
+	-not -name "*.connect.go" \
+	-print)
 goimports -w -l -local github.com/cszczepaniak/cribbly $non_templ_go_files
